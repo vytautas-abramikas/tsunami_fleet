207670dc2e46b95dfbe282c74898fe5af9519e2c
broliux <abramikas@gmail.com>
2024-10-21 02:44:06 +0300

    long messages for testing

--- src/components/ButtonBoard.tsx
+++ src/components/ButtonBoard.tsx
@@ -6,19 +6,31 @@ export const ButtonBoard: React.FC = () => {
   return (
     <div className="flex justify-center items-center pt-8 h-24">
       <button
-        onClick={() => addMessage({ text: "Maybe" })}
+        onClick={() =>
+          addMessage({
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+          })
+        }
         className="py-2 px-8 mr-4 ml-4 bg-indigo-600 hover:bg-indigo-700 hover:scale-110 transition duration-500 ease-out text-white text-center font-bold text-2xl rounded-lg shadow-lg"
       >
         Maybe
       </button>
       <button
-        onClick={() => addMessage({ text: "Yes" })}
+        onClick={() =>
+          addMessage({
+            text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+          })
+        }
         className="py-2 px-8 mr-4 ml-4 bg-green-600 hover:bg-green-700 hover:scale-110 transition duration-500 ease-out text-white text-center font-bold text-2xl rounded-lg shadow-lg"
       >
         Yes
       </button>
       <button
-        onClick={() => addMessage({ text: "No" })}
+        onClick={() =>
+          addMessage({
+            text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+          })
+        }
         className="py-2 px-8 mr-4 ml-4 bg-red-600 hover:bg-red-700 hover:scale-110 transition duration-500 ease-out text-white text-center font-bold text-2xl rounded-lg shadow-lg"
       >
         No
