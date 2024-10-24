b2e5cda790939b0c14c03a7a5c4cf479f583064d
broliux <abramikas@gmail.com>
2024-10-24 21:23:07 +0300

    renaming

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -4,21 +4,19 @@ import { Grid } from "./Grid";
 export const GameBoard: React.FC = () => {
   const { appState, activeCombatant } = useGameContext();
 
-  const showBrowserGrid: boolean =
+  const isBattle: boolean =
     appState === "BattleStart" ||
     appState === "Battle" ||
     appState === "BattleOver";
   return (
     <>
       <div
-        className={`grid ${
-          showBrowserGrid ? "grid-cols-2 gap-24" : "grid-cols-1"
-        }`}
+        className={`grid ${isBattle ? "grid-cols-2 gap-24" : "grid-cols-1"}`}
       >
         <div className="flex flex-col items-center justify-center">
           <h2
             className={`font-bold mb-4 drop-shadow-lg ${
-              showBrowserGrid && activeCombatant === "User"
+              isBattle && activeCombatant === "User"
                 ? "text-yellow-300 text-3xl"
                 : "text-white text-2xl"
             }`}
@@ -27,11 +25,11 @@ export const GameBoard: React.FC = () => {
           </h2>
           <Grid owner="User" />
         </div>
-        {showBrowserGrid && (
+        {isBattle && (
           <div className="flex flex-col items-center justify-center">
             <h2
               className={`font-bold mb-4 drop-shadow-lg ${
-                showBrowserGrid && activeCombatant === "Browser"
+                isBattle && activeCombatant === "Browser"
                   ? "text-yellow-300 text-3xl"
                   : "text-white text-2xl"
               }`}
