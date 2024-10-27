e9b32bd16b932d3c38451ffab43ab18d80a172a5
broliux <abramikas@gmail.com>
2024-10-27 03:06:52 +0200

    added a minimal logo effect

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,7 +1,7 @@
 export const Welcome: React.FC = () => {
   return (
     <>
-      <h1 className="font-extrabold text-9xl text-center drop-shadow-lg">
+      <h1 className="font-extrabold text-9xl text-center drop-shadow-lg logo">
         Tsunami Fleet
       </h1>
     </>
