5b2956e12e3759af91d3242a8f3a1d07bc46f8c2
broliux <abramikas@gmail.com>
2024-10-28 04:10:48 +0200

    created getUserShotResults function to do grid recalculation, handleUserShot only deals with state updates now

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -18,11 +18,9 @@ import { getGridWithShipsVisible } from "../utils/getGridWithShipsVisible";
 import { getWhoGetsFirstTurn } from "../utils/getWhoGetsFirstTurn";
 import { getBrowserShotResults } from "../utils/getBrowserShotResults";
 import { isLastSegmentToSinkOnGrid } from "../utils/isLastSegmentToSinkOnGrid";
-import { isLastSegment } from "../utils/isLastSegment";
-import { getShipCells } from "../utils/getShipCells";
-import { getShipNeighborCells } from "../utils/getShipNeighborCells";
 import { MSG_LIB, fillIn } from "../constants/MSG_LIB";
 import { BROWSER_TURN_TIMEOUT } from "../constants/BROWSER_TURN_TIMEOUT";
+import { getUserShotResults } from "../utils/getUserShotResults";
 
 export const GameContext = createContext<TGameContext | null>(null);
 
@@ -92,6 +90,7 @@ export const GameProvider: React.FC<{ children: ReactNode }> = ({
         setGridActiveStatus("Browser", "activate");
         setAddMessage(fillIn(MSG_LIB.UsersTurn, [activeCombatant]));
       } else {
+        //browser is gonna shoot now
         setTimeout(() => {
           const isUsersLastSegment = isLastSegmentToSinkOnGrid(userGrid);
           setGridActiveStatus("Browser", "deactivate");
@@ -135,64 +134,28 @@ export const GameProvider: React.FC<{ children: ReactNode }> = ({
 
   // handle User part of battle
   const handleUserShot = (cellId: number) => {
-    const cell = browserGrid[cellId];
     const isBrowsersLastSegment = isLastSegmentToSinkOnGrid(browserGrid);
     // console.log(JSON.stringify(cell));
-    let updatedCells: TCell[] = [];
-    if (cell.id === cellId && !cell.isVisible) {
-      if (cell.status === "ship") {
-        if (isLastSegment(cellId, browserShips, browserGrid)) {
-          //if last ship segment is hit, mark all ship segments as sunk
-          // console.log(
-          //   "User shooting, last segment of a ship detected, cellId: ",
-          //   cellId
-          // );
-          const sunkCells: TCell[] = getShipCells(
-            cell.shipId,
-            browserShips,
-            browserGrid
-          ).map((cell) => ({
-            ...cell,
-            status: "sunk" as "sunk",
-            isVisible: true,
-          }));
-          //mark neighboring cells of a sunk ship as visible
-          const revealedNeighbors: TCell[] = getShipNeighborCells(
-            cell.shipId,
-            browserShips,
-            browserGrid
-          ).map((cell) => ({ ...cell, isVisible: true }));
-          //merge all updated cells into one array, display message
-          updatedCells = [...sunkCells, ...revealedNeighbors];
-          if (isBrowsersLastSegment) {
-            // console.log("---last segment on grid is to be sunk by User ----");
-            setAddMessage(MSG_LIB.UserVictory);
-            // console.log("User shooting, BattleOver is to be set ", cellId);
-            setAppState("BattleOver");
-          } else {
-            // ship sunk, but it is not the last one
-            setAddMessage(MSG_LIB.UserSankBrowserShip);
-          }
-        } else {
-          //if the segment hit is not the last one of its ship
-          const updatedCell = {
-            ...cell,
-            status: "hit" as "hit",
-            isVisible: true,
-          };
-          updatedCells = [updatedCell];
-          setAddMessage(MSG_LIB.UserHitBrowserShip);
-        }
+    const { userHitStatus, cellsToProcess } = getUserShotResults(
+      cellId,
+      browserGrid,
+      browserShips
+    );
+    setUpdateGrid("Browser", cellsToProcess);
+
+    if (userHitStatus === "empty") {
+      setAddMessage(fillIn(MSG_LIB.UserMissBrowser, ["Browser"]));
+      setActiveCombatant("Browser");
+    } else if (userHitStatus === "hit") {
+      setAddMessage(MSG_LIB.UserHitBrowserShip);
+    } else {
+      if (!isBrowsersLastSegment) {
+        setAddMessage(MSG_LIB.UserSankBrowserShip);
       } else {
-        //if an empty cell is hit, just reveal it
-        const updatedCell = { ...cell, isVisible: true };
-        updatedCells = [updatedCell];
-        setAddMessage(fillIn(MSG_LIB.UserMissBrowser, ["Browser"]));
-        setActiveCombatant("Browser");
+        setAddMessage(MSG_LIB.UserVictory);
+        setAppState("BattleOver");
       }
     }
-    //set new grid state
-    setUpdateGrid("Browser", updatedCells);
   };
 
   //Setter for grid, updating some cells
---
+++ src/utils/getUserShotResults.ts
@@ -0,0 +1,60 @@
+import { TCell, TGrid, TShips } from "../types/types";
+import { isLastSegment } from "./isLastSegment";
+import { getShipCells } from "./getShipCells";
+import { getShipNeighborCells } from "./getShipNeighborCells";
+
+export const getUserShotResults = (
+  cellId: number,
+  grid: TGrid,
+  ships: TShips
+): { userHitStatus: "empty" | "hit" | "sunk"; cellsToProcess: TCell[] } => {
+  let userHitStatus: "empty" | "hit" | "sunk" = "empty";
+  let cellsToProcess: TCell[] = [];
+  const cell: TCell = grid[cellId];
+
+  if (cell.id === cellId && !cell.isVisible) {
+    if (cell.status === "ship") {
+      if (isLastSegment(cellId, ships, grid)) {
+        //if last ship segment is hit, mark all ship segments as sunk
+        // console.log(
+        //   "User shooting, last segment of a ship detected, cellId: ",
+        //   cellId
+        // );
+        userHitStatus = "sunk" as "sunk";
+        const sunkCells: TCell[] = getShipCells(cell.shipId, ships, grid).map(
+          (cell) => ({
+            ...cell,
+            status: "sunk" as "sunk",
+            isVisible: true,
+          })
+        );
+        //mark neighboring cells of a sunk ship as visible
+        const revealedNeighbors: TCell[] = getShipNeighborCells(
+          cell.shipId,
+          ships,
+          grid
+        ).map((cell) => ({ ...cell, isVisible: true }));
+        //merge all updated cells into one array, display message
+        cellsToProcess = [...sunkCells, ...revealedNeighbors];
+      } else {
+        //if the segment hit is not the last one of its ship
+        userHitStatus = "hit" as "hit";
+        const updatedCell = {
+          ...cell,
+          status: "hit" as "hit",
+          isVisible: true,
+        };
+        cellsToProcess = [updatedCell];
+      }
+    } else {
+      //if an empty cell is hit, just reveal it
+      const updatedCell = { ...cell, isVisible: true };
+      cellsToProcess = [updatedCell];
+    }
+  } else {
+    console.error(
+      `Illegal action on user click, cellId:${cellId}, grid:${grid}, ships:${ships}`
+    );
+  }
+  return { userHitStatus: userHitStatus, cellsToProcess: cellsToProcess };
+};
