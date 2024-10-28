bea76cbb937579288d85daf49c2a9d87dcb3c7c7
broliux <abramikas@gmail.com>
2024-10-28 14:07:55 +0200

    different cursor style on grid when Browser is shooting

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useMemo } from "react";
 import { useGameContext } from "../hooks/useGameContext";
 import { Cell } from "./Cell";
 import { TCombatant, TGrid } from "../types/types";
@@ -8,6 +8,16 @@ export const Grid: React.FC<{ owner: TCombatant; grid: TGrid }> = ({
   grid,
 }) => {
   const { appState, activeCombatant, handleUserShot } = useGameContext();
+
+  const pointerStyle = useMemo(() => {
+    if (appState === "Battle" || appState === "BattlePause") {
+      if (activeCombatant === "Browser") {
+        return "cursor-wait";
+      }
+    } else {
+      return "";
+    }
+  }, [appState, activeCombatant]);
 
   const handleCellClick = (cellId: number) => {
     if (
@@ -20,7 +30,7 @@ export const Grid: React.FC<{ owner: TCombatant; grid: TGrid }> = ({
   };
 
   return (
-    <div key={owner} className="grid grid-cols-10 gap-1">
+    <div key={owner} className={`grid grid-cols-10 gap-1 ${pointerStyle}`}>
       {grid.map((cell) => (
         <Cell key={cell.id} cell={cell} onClick={handleCellClick} />
       ))}
