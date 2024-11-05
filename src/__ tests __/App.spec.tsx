f4591c3f0f05ebbd2e4918658ed9f99508c0e1f3
broliux <abramikas@gmail.com>
2024-11-06 00:03:01 +0200

    one test ready

--- src/__ tests __/App.spec.tsx
+++ src/__ tests __/App.spec.tsx
@@ -3,15 +3,41 @@ import { render } from "@testing-library/react";
 import { GameProvider } from "../context/GameContext";
 import { App } from "../components/App";
 
-test("demo", () => {
-  expect(true).toBe(true);
-});
-
-test("Renders the main page", () => {
-  render(
-    <GameProvider>
-      <App />
-    </GameProvider>
-  );
-  expect(true).toBeTruthy();
+describe("App initial components are rendered", () => {
+  it("has a correct heading", () => {
+    const { getByRole } = render(
+      <GameProvider>
+        <App />
+      </GameProvider>
+    );
+    expect(getByRole("heading", { name: "Tsunami Fleet" })).toBeInTheDocument();
+  });
+  it("has a correct message", () => {
+    const { getByText } = render(
+      <GameProvider>
+        <App />
+      </GameProvider>
+    );
+    expect(getByText("Dare to brave the seas?")).toBeInTheDocument();
+  });
+  it("has a correct button", () => {
+    const { getByRole } = render(
+      <GameProvider>
+        <App />
+      </GameProvider>
+    );
+    expect(
+      getByRole("button", { name: "Prepare for battle" })
+    ).toBeInTheDocument();
+  });
+  it("has correct copyright info", () => {
+    const { getByText } = render(
+      <GameProvider>
+        <App />
+      </GameProvider>
+    );
+    expect(
+      getByText("Tsunami Fleet © 2024 by Vytautas Abramikas")
+    ).toBeInTheDocument();
+  });
 });
