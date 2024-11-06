6cadbf6e84d834a4aca09e003e8e26e204f2be8d
broliux <abramikas@gmail.com>
2024-11-06 17:57:21 +0200

    GameBoard logic refactor and combatant titles recolor

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -5,12 +5,6 @@ export const GameBoard: React.FC = () => {
   const { appState, activeCombatant, playerGrid, browserGrid } =
     useGameContext();
 
-  const isBattle: boolean =
-    appState === "BattleStart" ||
-    appState === "Battle" ||
-    appState === "BattlePause" ||
-    appState === "BattleOver";
-
   const hidePlayerTitle: boolean =
     appState === "PlacementStart" ||
     appState === "PlacementEmpty" ||
@@ -19,33 +13,56 @@ export const GameBoard: React.FC = () => {
     appState === "PlacementFinalize" ||
     appState === "PlacementGenerate";
 
+  const isBattle: boolean =
+    appState === "BattleStart" ||
+    appState === "Battle" ||
+    appState === "BattlePause";
+
+  const isBattleOver: boolean = appState === "BattleOver";
+
+  const isShowBothGrids: boolean = isBattle || isBattleOver;
+
+  const playerTitleClasses: string = hidePlayerTitle
+    ? "opacity-0"
+    : isBattle
+    ? activeCombatant === "Player"
+      ? "text-white"
+      : "text-white opacity-50"
+    : isBattleOver
+    ? activeCombatant === "Player"
+      ? "text-yellow-300"
+      : "text-[#8B0000]"
+    : "";
+
+  const browserTitleClasses: string = isBattle
+    ? activeCombatant === "Browser"
+      ? "text-white"
+      : "text-white opacity-50"
+    : isBattleOver
+    ? activeCombatant === "Browser"
+      ? "text-yellow-300"
+      : "text-[#8B0000]"
+    : "";
+
   return (
     <>
       <div
-        className={`grid ${isBattle ? "grid-cols-2 gap-24" : "grid-cols-1"}`}
+        className={`grid ${
+          isShowBothGrids ? "grid-cols-2 gap-24" : "grid-cols-1"
+        }`}
       >
         <div className="flex flex-col items-center justify-center">
           <h2
-            className={`flex items-end font-bold mb-4 drop-shadow-lg h-10 ${
-              hidePlayerTitle ? "opacity-0" : ""
-            } ${
-              isBattle && activeCombatant === "Player"
-                ? "text-yellow-300 text-3xl"
-                : "text-white text-2xl"
-            }`}
+            className={`flex items-end font-bold mb-4 drop-shadow-lg h-10 text-3xl ${playerTitleClasses}`}
           >
             Player
           </h2>
           <Grid owner="Player" grid={playerGrid} />
         </div>
-        {isBattle && (
+        {isShowBothGrids && (
           <div className="flex flex-col items-center justify-center new-message">
             <h2
-              className={`flex items-end font-bold mb-4 drop-shadow-lg h-10 ${
-                isBattle && activeCombatant === "Browser"
-                  ? "text-yellow-300 text-3xl"
-                  : "text-white text-2xl"
-              }`}
+              className={`flex items-end font-bold mb-4 drop-shadow-lg h-10 text-3xl ${browserTitleClasses}`}
             >
               Browser
             </h2>
